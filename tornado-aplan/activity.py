cb30db410d1a3b7af84728875a5721b8bb1c4a51
ThomasZhang <thomas.zh@qq.com>
2016-03-05 17:27:19 +0800

    init aplan project

---
+++ tornado-aplan/activity.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+#
+# Copyright 2015 planc2c.com
+# thomas@time2box.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+import logging
+
+from tornado.escape import json_decode, json_encode
+from tornado.httpclient import HTTPClient
+from tornado.httputil import url_concat
+
+from account import ssoLogin
+from base import BaseHandler, timestamp_datetime
+
+
+APP_ID = "wxaa328c83d3132bfb"
+APP_SECRET = "32bbf99a46d80b24bae81e8c8558c42f"
+DOMAIN = "planc2c.com"
+STP = "123.56.228.41"
+
+
+def getAccessToken(appId, appSecret, code):
+    url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid="+appId+"&secret="+appSecret+"&code="+code+"&grant_type=authorization_code"
+    http_client = HTTPClient()
+    response = http_client.fetch(url, method="GET")
+    logging.info("got response %r", response.body)
+    accessToken = json_decode(response.body)
+    return accessToken
+
+
+def getUserInfo(token, openid):
+    url = "https://api.weixin.qq.com/sns/userinfo?access_token="+token+"&openid="+openid+"&lang=zh_CN"
+    http_client = HTTPClient()
+    response = http_client.fetch(url, method="GET")
+    logging.info("got response %r", response.body)
+    userInfo = json_decode(response.body)
+    return userInfo
+
+
+class ActivityInfoHandler(BaseHandler):
+    def get(self):
+        _id = self.get_argument("ekey", "")
+        #self.render('wechat/activity_info.html', ekey=_id)
+        self.redirect("https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxaa328c83d3132bfb&redirect_uri=http://planc2c.com/activity/desc?ekey="+_id+"&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect")
+
+class ActivityApplyHandler(BaseHandler):
+    def get(self):
+        _id = self.get_argument("ekey", "")
+        _sessionTicket = self.get_secure_cookie("ticket")
+        
+        params = {"X-Session-Id": _sessionTicket}
+        url = url_concat("http://"+STP+"/activities/"+_id+"/detail", params)
+        http_client = HTTPClient()
+        response = http_client.fetch(url, method="GET")
+        logging.info("got response %r", response.body)
+        _info = json_decode(response.body)
+        
+        _begin_time = timestamp_datetime(_info["beginTime"]/1000)
+        _info["beginTime"] = _begin_time
+        
+        params = {"X-Session-Id": _sessionTicket}
+        url = url_concat("http://"+STP+"/activities/"+_id+"/application-template", params)
+        http_client = HTTPClient()
+        response = http_client.fetch(url, method="GET")
+        logging.info("got response %r", response.body)
+        _template = json_decode(response.body)
+        
+        self.render('wechat/activity_apply.html', ekey=_id, info = _info, template = _template)
+
+    def post(self):
+        _id = self.get_argument("ekey", "")
+        _sessionTicket = self.get_secure_cookie("ticket")
+        
+        params = {"X-Session-Id": _sessionTicket}
+        url = url_concat("http://"+STP+"/activities/"+_id+"/application-template", params)
+        http_client = HTTPClient()
+        response = http_client.fetch(url, method="GET")
+        logging.info("got response %r", response.body)
+        _template = json_decode(response.body)
+        
+        arr = []
+        for _field in _template:
+            _field_name = _field['fieldName']
+            _field_name = unicode(_field_name).encode('utf-8')
+            logging.info("got fieldName %r", _field_name)
+            _field_value = self.get_argument("participation_"+_field_name, "")
+            logging.info("got fieldValue %r", _field_value)
+            data = {_field_name : _field_value, "type": 200}
+            arr.append(data)
+        _json = json_encode(arr)
+        
+        params = {"X-Session-Id": _sessionTicket}
+        url = url_concat("http://"+STP+"/activities/"+_id+"/applications", params)
+        http_client = HTTPClient()
+        response = http_client.fetch(url, method="POST", body=_json)
+        
+        self.render('wechat/activity_apply_success.html')
+
+
+class ActivityDescHandler(BaseHandler):
+    def get(self):
+        _id = self.get_argument("ekey", "")
+        logging.debug("got id %r", _id)
+        _code = self.get_argument("code", "")
+        logging.debug("got code %r", _code)
+        _unionid = self.get_argument("unionid", "")
+        logging.debug("got unionid %r", _unionid)
+        
+#        _sessionTicket = self.get_secure_cookie("ticket")
+#        if not _sessionTicket:
+        accessToken = getAccessToken(APP_ID, APP_SECRET, _code);
+        _token = accessToken["access_token"];
+        logging.debug("got token %r", _token)
+        _openid = accessToken["openid"];
+        logging.debug("got openid %r", _openid)
+        _unionid = accessToken["unionid"];
+        logging.debug("got unionid %r", _unionid)
+        
+        userInfo = getUserInfo(_token, _openid)
+        _nickname = userInfo["nickname"]
+        _nickname = unicode(_nickname).encode('utf-8')
+        logging.debug("got nickname %r", _nickname)
+        _headimgurl = userInfo["headimgurl"]
+        logging.debug("got headimgurl %r", _headimgurl)
+        
+        _user_agent = self.request.headers["User-Agent"]
+        _lang = self.request.headers["Accept-Language"]
+        # 1604=wechat
+        stpSession = ssoLogin(1604, _unionid, _nickname, _headimgurl, _user_agent, _lang)
+        _accountId = stpSession["accountId"]
+        _sessionTicket = stpSession["sessionToken"]
+        self.set_secure_cookie("ticket", _sessionTicket)
+        self.set_secure_cookie("accountId", _accountId)
+        self.set_secure_cookie("nickname", _nickname)
+
+        params = {"X-Session-Id": _sessionTicket}
+        url = url_concat("http://"+STP+"/activities/"+_id+"/detail", params)
+        http_client = HTTPClient()
+        response = http_client.fetch(url, method="GET")
+        logging.info("got response %r", response.body)
+        _info = json_decode(response.body)
+        
+        _begin_time = timestamp_datetime(_info["beginTime"]/1000)
+        _info["beginTime"] = _begin_time
+         
+        params = {"X-Session-Id": _sessionTicket}
+        url = url_concat("http://"+STP+"/activities/"+_id+"/poster", params)
+        http_client = HTTPClient()
+        response = http_client.fetch(url, method="GET")
+        logging.info("got response %r", response.body)
+        _descs = json_decode(response.body)
+        
+        self.render('wechat/activity_desc.html', ekey = _id, info = _info, descs = _descs)
---
+++ tornado-aplan/main.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+#
+# Copyright 2015 planc2c.com
+# thomas@time2box.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+import os.path
+
+from tornado.httpclient import HTTPClient
+import tornado.ioloop
+from tornado.options import define, options
+import tornado.web
+
+from account import LoginHandler, LogoutHandler, RegisterHandler, \
+    ForgotPwdHandler, ResetPwdHandler
+from activity import ActivityDescHandler, ActivityInfoHandler, \
+    ActivityApplyHandler
+
+
+define("port", default=8888, help="run on the given port", type=int)
+define("debug", default=True, help="run in debug mode")
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('index.html')
+
+
+class AgreementHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('agreement.html')
+
+
+class PrivacyHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('privacy.html')
+
+
+class ContactHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('contact.html')
+
+
+class PageNotFoundHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('404.html')
+
+
+class JourneyHandler(tornado.web.RequestHandler):
+    def get(self):
+        APP_ID = "wxaa328c83d3132bfb";
+        APP_SECRET = "32bbf99a46d80b24bae81e8c8558c42f";
+        DOMAIN = "planc2c.com";
+        
+        http_client = HTTPClient()
+        oauth_url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + APP_ID + "&redirect_uri=http://planc2c.com&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect" 
+        response = http_client.fetch(oauth_url)
+        str = response.body
+        print str.decode('iso8859-1')
+        
+        self.render('index.html')
+
+
+def main():
+    app = tornado.web.Application(
+        [
+            (r'/', MainHandler),
+            (r'/contact', ContactHandler),
+            (r'/agreement', AgreementHandler),
+            (r'/privacy', PrivacyHandler),
+            (r'/account/login', LoginHandler),
+            (r'/account/logout', LogoutHandler),
+            (r'/account/register', RegisterHandler),
+            (r'/account/forgot-pwd', ForgotPwdHandler),
+            (r'/account/reset-pwd', ResetPwdHandler),
+            (r'/activity/desc', ActivityDescHandler),
+            (r'/activity/info', ActivityInfoHandler),
+            (r'/activity/apply', ActivityApplyHandler),
+            (r'/stp-web-5.0.0/aplan/activity/info', ActivityInfoHandler),
+            (".*", PageNotFoundHandler),
+        ],
+        # __TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__
+        cookie_secret="bZJc2sWbQLKos6GkHn/VB9oXwQt8S0R0kRvJ5/xJ89E=",
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        static_path=os.path.join(os.path.dirname(__file__), "static"),
+        xsrf_cookies=True,
+        debug=options.debug,
+        login_url="/account/login",
+    )
+    #tornado.locale.load_gettext_translations(os.path.join(os.path.dirname(__file__), "locale"), "aplan")
+    tornado.locale.set_default_locale("en_US")
+    tornado.options.parse_command_line()
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
+
+if __name__ == '__main__':
+    main()
